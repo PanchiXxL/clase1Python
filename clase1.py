81997f7266de0e64290e0fd793bdee783c337ba4
Edison <molinaedison5@gmail.com>
2020-11-18 06:56:48 -0500

    ciclo for

--- clase1.py
+++ clase1.py
@@ -1,3 +1,10 @@
 print ("hola mundo")
-a = int(input("Ingrese numero: "))
-print("el nuemro ingresao es: ",a)  
+a = int(input("Ingrese inicio: "))
+
+b = int(input("Ingrese fin: "))
+
+c = int(input("Ingrese valor para la table de multiplicar"))
+
+for i in range(a,b):
+    mult = i*c
+    print(i," * ",c," = ",mult)
