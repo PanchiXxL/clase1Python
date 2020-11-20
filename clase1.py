c0db07933c21bf0d13c765751733dbdf3342a515
Edison <molinaedison5@gmail.com>
2020-11-19 22:59:30 -0500

    hola

--- clase1.py
+++ clase1.py
@@ -1,7 +1,7 @@
-print ("hola mundo")
-'''a = int(input("Ingrese inicio: "))
+'''print ("hola mundo")
+a = int(input("Ingrese inicio: "))
 
-b = int(input("Ingrese fin: "))'''
+b = int(input("Ingrese fin: "))
 
 c = int(input("Ingrese valor para la table de multiplicar: "))
 cont=0
@@ -16,4 +16,28 @@ for i in range(1,11):
 print("la suma de las respuestas pares es: ",cont)
 print("Suma total: ",d)
 
+                ARREGLOS
+
+
+format -> sirve para una concatenacion 
+insert -> nos permite insertar un valor en cualquier parte deun arreglo  
+len -> permite ver la cantidad de datos de un vector             
+                '''
+
+n = int(input("Ingrese dimension del vector: "))
+
+vect = []
+for i in range(0, n):
+    valor = int(input("ingrese valores: ").format(i))
+    vect.append(valor)
+print("Los valores ingresados son")
+
+for i in range (0, n):
+    print("[ ",i," ]",vect[i])
+
+print("ingrese nuevos elementos en el arreglo: ")
+vect.insert(2,10)
+print("valores finales son: ")
+for i in range(0, len(vect)):
+    print("[ ",i," ]",vect[i])
 
